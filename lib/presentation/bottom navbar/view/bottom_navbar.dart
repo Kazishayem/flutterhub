1328ed37f45e8ee1790a250c33773cb1baec5704
kazishayem <kazishayem1234@gmail.com>
2026-03-11 19:59:47 +0600

    ui

--- lib/presentation/bottom navbar/view/bottom_navbar.dart
+++ lib/presentation/bottom navbar/view/bottom_navbar.dart
@@ -5,7 +5,6 @@ import 'package:flutterhub/core/resource/constansts/icon_manager.dart';
 import 'package:flutterhub/presentation/post/view/post_screen.dart';
 import 'package:flutterhub/presentation/product/view/product_screen.dart';
 import 'package:flutterhub/presentation/setting/view/setting_screen.dart';
-
 import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 class BottomNavbar extends StatefulWidget {
@@ -33,56 +32,45 @@ class _BottomNavbarState extends State<BottomNavbar> {
       body: pages[selectedPage],
       bottomNavigationBar: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
+          padding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 8.0),
           child: SalomonBottomBar(
             currentIndex: selectedPage,
             onTap: onPageTapped,
             items: [
               SalomonBottomBarItem(
                 icon: Image.asset(
-                  IconManager.home,
+                  IconManager.shop,
                   height: 30.h,
                   width: 30.w,
                   fit: BoxFit.contain,
+                  color: ColorManager.errorColor,
                 ),
                 title: const Text("Product"),
-                selectedColor: ColorManager.textRedColor,
+                selectedColor: ColorManager.errorColor,
               ),
               SalomonBottomBarItem(
                 icon: Image.asset(
-                  IconManager.bell,
+                  IconManager.profile,
                   height: 30.h,
                   width: 30.w,
                   fit: BoxFit.contain,
-                  color: ColorManager.textRedColor,
+                  color: ColorManager.textGreenColor,
                 ),
                 title: const Text("Post"),
-                selectedColor: ColorManager.textRedColor,
+                selectedColor: ColorManager.textGreenColor,
               ),
               SalomonBottomBarItem(
                 icon: Image.asset(
-                  IconManager.home,
+                  IconManager.setting,
                   height: 30.h,
                   width: 30.w,
                   fit: BoxFit.contain,
-                  color: ColorManager.textRedColor,
+                  color: ColorManager.borderColor1,
                 ),
                 title: const Text("Setting"),
 
-                selectedColor: ColorManager.textRedColor,
+                selectedColor: ColorManager.borderColor1,
               ),
-              // SalomonBottomBarItem(
-              //   icon: Image.asset(
-              //     IconManager.bell,
-              //     height: 30.h,
-              //     width: 30.w,
-              //     fit: BoxFit.contain,
-              //     color: ColorManager.textRedColor,
-              //   ),
-              //   title: const Text("Profile"),
-
-              //   selectedColor: ColorManager.textRedColor,
-              // ),
             ],
           ),
         ),
--- lib/presentation/common widget/common_header.dart
+++ lib/presentation/common widget/common_header.dart
@@ -1,64 +1,64 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutterhub/core/resource/constansts/color_manger.dart';
-import 'package:flutterhub/core/resource/constansts/icon_manager.dart';
-import 'package:flutterhub/core/resource/style_manager.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:flutterhub/core/resource/constansts/color_manger.dart';
+// import 'package:flutterhub/core/resource/constansts/icon_manager.dart';
+// import 'package:flutterhub/core/resource/style_manager.dart';
 
-class CommonHeader extends StatelessWidget {
-  final VoidCallback? onBackTap;
-  final VoidCallback? onLanguageTap;
-  final bool showBack;
-  final bool showLanguage;
-  final String languageText;
+// class CommonHeader extends StatelessWidget {
+//   final VoidCallback? onBackTap;
+//   final VoidCallback? onLanguageTap;
+//   final bool showBack;
+//   final bool showLanguage;
+//   final String languageText;
 
-  const CommonHeader({
-    super.key,
-    this.onBackTap,
-    this.onLanguageTap,
-    this.showBack = true,
-    this.showLanguage = true,
-    this.languageText = "BN",
-  });
+//   const CommonHeader({
+//     super.key,
+//     this.onBackTap,
+//     this.onLanguageTap,
+//     this.showBack = true,
+//     this.showLanguage = true,
+//     this.languageText = "BN",
+//   });
 
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        if (showBack)
-          InkWell(
-            onTap: onBackTap ?? () => Navigator.pop(context),
-            child: Image.asset(
-              IconManager.arrowleft,
-              height: 20.h,
-              width: 20.w,
-              fit: BoxFit.contain,
-            ),
-          ),
+//   @override
+//   Widget build(BuildContext context) {
+//     return Row(
+//       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//       children: [
+//         if (showBack)
+//           InkWell(
+//             onTap: onBackTap ?? () => Navigator.pop(context),
+//             child: Image.asset(
+//               IconManager.arrowleft,
+//               height: 20.h,
+//               width: 20.w,
+//               fit: BoxFit.contain,
+//             ),
+//           ),
 
-        if (showLanguage) ...[
-          SizedBox(width: 12.w),
-          InkWell(
-            onTap: onLanguageTap,
-            child: Container(
-              width: 50.w,
-              height: 30.h,
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30.r),
-                color: ColorManager.textRedColor,
-              ),
-              child: Text(
-                languageText,
-                style: getMedium500Style14(
-                  fontSize: 14.sp,
-                  color: ColorManager.whiteColor,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ],
-    );
-  }
-}
+//         if (showLanguage) ...[
+//           SizedBox(width: 12.w),
+//           InkWell(
+//             onTap: onLanguageTap,
+//             child: Container(
+//               width: 50.w,
+//               height: 30.h,
+//               alignment: Alignment.center,
+//               decoration: BoxDecoration(
+//                 borderRadius: BorderRadius.circular(30.r),
+//                 color: ColorManager.textRedColor,
+//               ),
+//               child: Text(
+//                 languageText,
+//                 style: getMedium500Style14(
+//                   fontSize: 14.sp,
+//                   color: ColorManager.whiteColor,
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ],
+//     );
+//   }
+// }
